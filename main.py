a72cd49203108190ba1fe50ec6aea655c5f3090b
kaylees22 <ksweet1009@gmail.com>
2024-06-27 17:53:32 -0400

    new display features

--- main.py
+++ main.py
@@ -1,25 +1,61 @@
 from spotify import Spotify
+import pandas as pd
+import sqlalchemy as db
+
+
+def display(list_of_dicts):
+    
+    dataframe = pd.DataFrame.from_records(list_of_dicts) 
+
+    # engine connects to db
+    engine = db.create_engine('sqlite:///releases.db')
+    dataframe.to_sql('releasesTable', con=engine, if_exists='replace', index=False)
+
+
+    with engine.connect() as connection:
+        query_result = connection.execute(db.text("SELECT * FROM releasesTable;")).fetchall()
+        print(pd.DataFrame(query_result))
+    
+    return
+
 
 def main():
     spotify_api = Spotify()
+    print('\nWelcome to My Idol Update! Use this tool to see your favorite artist\'s recent releases and tweets\n')
 
-    while True:
-        artist = input("Please input an artist name (q to quit): ")
+    quit = False
+    while not quit:
+        
+        artist = input("Please enter an artist name or q to quit: ")
         if artist == 'q':
+            quit = True
             break
-
+        
         artist_id = spotify_api.get_artist_id(artist)
+
+        # prompt until success
+        while artist_id is None:
+            artist = input('Sorry. we couldn\'t find that artist. Please enter an artist name or q to quit: ')
+
+            if artist == 'q':
+                quit = True
+                break
+
+            artist_id = spotify_api.get_artist_id(artist)
+        if quit: break
+        
+        official_name = spotify_api.get_artist_name(artist_id)
         releases = spotify_api.get_artist_releases(artist_id)
 
-        print(f"Here are {artist}'s recent releases")
-        print("---------------------------------------")
-        for name, date, type in releases:
-            print(f'{name} is a(n) {type} that was released on {date}')
-    
-    return
-    
+        print(f"\n{official_name}'s recent releases")
+        separator = ''
+        print(separator.join(['-']*(len(official_name) + 18))) # logic to change len of separator based on name length
 
-
+        display(releases)
+        print()
+        
+    print('Goodbye!\n')
+    
 
 if __name__ == '__main__':
     main()
--- spotify.py
+++ spotify.py
@@ -19,31 +19,36 @@ class Spotify:
         self.access_token = self.auth_response_data['access_token']
         self.headers = {'Authorization': 'Bearer {token}'.format(token=self.access_token)}
 
+    def get_artist_name(self, artist_id):
+        response = requests.get(url=BASE_URL + f'artists/{artist_id}',headers = self.headers)
+        return response.json()['name']
+
 
 
     def get_artist_id(self, artist_name):
         """
         get artist id/info
         """
+        try:
+            # search for the artist
+            response = requests.get(url=BASE_URL + 'search/',headers = self.headers, params={'q': artist_name, 'type': 'artist'})
+            data = response.json()
+            res = data['artists']['items'][0]['id']
+            return res
+        except:
+            return None
         
-        # search for the artist
-        response = requests.get(url=BASE_URL + 'search/',headers = self.headers, params={'q': artist_name, 'type': 'artist'})
-        data = response.json()
 
-
-        return data['artists']['items'][0]['id']
-        
-
-    def get_artist_releases(self, id):
+    def get_artist_releases(self, aid):
         """
             get 5 most recent releases from artist with given id
         """
 
-        response = requests.get(url=BASE_URL + f'artists/{id}/albums',headers = self.headers, params={'include_groups': 'album,single,appears_on'})
+        response = requests.get(url=BASE_URL + f'artists/{aid}/albums',headers = self.headers, params={'include_groups': 'album,single,appears_on'})
         data = response.json()
         
         sorted_releases = sorted(data['items'], key=lambda item: item['release_date'], reverse=True)[:5]
-        releases = [[item['name'], item['release_date'], item['album_type']] for item in sorted_releases]
+        releases = [{'name': item['name'], 'release date': item['release_date'], 'type': item['album_type']} for item in sorted_releases]
 
         return releases
 
@@ -51,4 +56,4 @@ class Spotify:
 if __name__ == '__main__':
     spotify = Spotify()
     spotify.get_artist_releases(spotify.get_artist_id('drake'))
-    pass
+    print(spotify.get_artist_name('74KM79TiuVKeVCqs8QtB0B'))
